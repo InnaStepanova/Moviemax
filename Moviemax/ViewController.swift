16ecddc2a92c34f3210c1b0de0b6bb1f610de138
InnaStepanova <inna-panichkina@yandex.ru>
2023-04-03 11:00:48 +0300

    Initial Commit

---
+++ Moviemax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moviemax
+//
+//  Created by Лаванда on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
