cc67e4b727b06c9fe056905daa918c5d415e3557
nikolay <bareee@mail.ru>
2023-04-04 20:13:27 +0300

    1pravka

--- Moviemax/Application/SceneDelegate.swift
+++ Moviemax/Application/SceneDelegate.swift
@@ -16,8 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        let tabBarController = TabBarController()
-        tabBarController.selectedIndex = 2
+        let tabBarController = MovieDetail()
+//        tabBarController.selectedIndex = 2
         window?.rootViewController = tabBarController
 
     }
