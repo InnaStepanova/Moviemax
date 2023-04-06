87b7c37a0a0d575d3b7959215d5e5a0e16bd2058
InnaStepanova <inna-panichkina@yandex.ru>
2023-04-06 14:38:20 +0300

    Add CategoryCollectionView Add CategoryCollectionViewCell

---
+++ Moviemax/ViewControllers/CategoryCollectionView.swift
@@ -0,0 +1,67 @@
+//
+//  CategoryCollectionView.swift
+//  Moviemax
+//
+//  Created by Лаванда on 06.04.2023.
+//
+
+import UIKit
+
+class CategoryCollectionView: UICollectionView {
+    
+    var categories  = ["All", "Action", "Adventure", "Mystery", "Fantasy", "Others"]
+    
+    let collectionLayout = UICollectionViewFlowLayout()
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: .zero, collectionViewLayout: collectionLayout)
+        setDelegate()
+        collectionLayout.scrollDirection = .horizontal
+        showsHorizontalScrollIndicator = false
+        selectItem(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .left)
+        register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "CategoryCell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setDelegate() {
+        dataSource = self
+        delegate = self
+    }
+}
+
+//MARK: UICollectionViewDataSource
+
+extension CategoryCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as? CategoryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let category = categories[indexPath.item]
+        cell.set(buttonTitle: category)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //           что будет при нажатии на ячейку
+    }
+}
+
+//MARK: UICollectionViewDelegateFlowLayout
+
+extension CategoryCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let categoryFont = Resources.Fonts.plusJakartaSans(with: 12)
+        let categoryAttributes = [NSAttributedString.Key.font : categoryFont]
+        let categoryWidth = categories[indexPath.item].size(withAttributes: categoryAttributes).width + 48
+        return CGSize(width: categoryWidth, height: 34)
+    }
+}
