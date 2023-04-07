65d1424ee0825e9eb38722f7e37c27fd46567e72
InnaStepanova <inna-panichkina@yandex.ru>
2023-04-07 17:16:47 +0300

    no message

--- Moviemax/Application/SceneDelegate.swift
+++ Moviemax/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
         let tabBarController = StartViewController()
-//        tabBarController.selectedIndex = 2
+//        tabBarController.selectedIndex = 2 
         window?.rootViewController = ProfileSettingsVC()
 
     }
