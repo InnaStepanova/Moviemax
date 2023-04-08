53e6bcce260209827352e4bf512433910fa089de
InnaStepanova <inna-panichkina@yandex.ru>
2023-04-08 22:28:09 +0300

    Add MovieDetail

---
+++ Moviemax/SupportingFiles/Extensions/Image.swift
@@ -0,0 +1,36 @@
+//
+//  Image.swift
+//  Moviemax
+//
+//  Created by Лаванда on 08.04.2023.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    static var dateImage: UIImageView {
+        let image = UIImageView()
+        image.image = UIImage(named: "date")
+        return image
+    }
+    static var genreImage: UIImageView {
+        let image = UIImageView()
+        image.image = UIImage(named: "movie")
+        return image
+    }
+    
+    static var timeImage: UIImageView {
+        let image = UIImageView()
+        image.image = UIImage(named: "time")
+        return image
+    }
+    
+    static var starImage: UIImageView {
+        let image = UIImageView()
+        image.image = UIImage(named: "star")
+        return image
+    }
+    
+}
+
