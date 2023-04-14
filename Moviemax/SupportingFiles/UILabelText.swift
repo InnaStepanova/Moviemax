0c4d52f6b470652a730b867d5d6396f0ee823f99
Colokol <Colokol1@gmail.com>
2023-04-14 23:30:43 +0300

    add Change password VC

--- Moviemax/SupportingFiles/UILabelText.swift
+++ Moviemax/SupportingFiles/UILabelText.swift
@@ -41,3 +41,12 @@ extension UILabel {
     
 }
 
+//MARK: Добавлям метод для создания отступа слева от текстового поля
+extension UITextField {
+func paddingLeft(_ padding: CGFloat) {
+    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.height))
+    self.leftView = paddingView
+    self.leftViewMode = .always
+}
+}
+
