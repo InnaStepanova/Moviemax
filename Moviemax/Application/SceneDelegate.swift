eb7c12fe694bfa2d1e99e0d076f0b44fdfc9f96e
Colokol <Colokol1@gmail.com>
2023-04-21 13:22:40 +0300

    add anim to Start VC

--- Moviemax/Application/SceneDelegate.swift
+++ Moviemax/Application/SceneDelegate.swift
@@ -18,13 +18,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
+        window?.rootViewController = StartViewController()
         
-        if let _ = UserDefaults.standard.object(forKey: "FirstRun") as? Int {
-            window?.rootViewController = navController
-        }else{
-            window?.rootViewController = navController1
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            if let _ = UserDefaults.standard.object(forKey: "FirstRun") as? Int {
+                self.window?.rootViewController = navController
+            } else {
+                self.window?.rootViewController = navController1
+            }
         }
 
+        
         if let currentScheme = UserDefaults.standard.value(forKey: "theme") as? Int {
             window?.rootViewController?.overrideUserInterfaceStyle = currentScheme == 1 ? .light : .dark
         }
